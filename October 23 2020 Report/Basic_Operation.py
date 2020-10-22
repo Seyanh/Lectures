42caa96a25a0d6fee9cdd8ecd93f7de9e8232da5
Sunyuan <71776283+Seyanh@users.noreply.github.com>
2020-10-22 20:34:15 +0800

    Add files via upload

---
+++ October 23 2020 Report/Basic_Operation.py
@@ -0,0 +1,35 @@
+print("Hello")
+print("Hello")
+
+x = 5
+print(x +2)
+print(x - 2)
+print(x * 2)
+print(x / 2)
+print(x ** 2)
+print(x % 2)
+
+# Type Conversion , if unknown , help ( str )
+print(type(5))
+print(str(x))
+print(int(3.5))
+print(float(4))
+print(bool(3))
+ 
+my_string = "  Test Python"
+print(my_string + my_string)
+print(my_string * 2)
+
+# String operations
+print(my_string[0])
+print(my_string[0:3])
+my_string = " Test Python "
+print(my_string)
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.count("t"))
+print(my_string.strip()) # remove the preceding spaces
+print(my_string.replace("n", "ner"))
+print(my_string.index("T"))
+
+dir(my_string)
